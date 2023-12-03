ef983c7b266b734350973a6d37ac1eeb2c9407fb
JohnathanBenge <johnathanbenge@gmail.com>
2023-12-03 13:58:37 -0500

    config

--- flake.nix
+++ flake.nix
@@ -82,7 +82,6 @@
         system = system;
         specialArgs = inputs;
         modules = [
-          disko.nixosModules.disko
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
