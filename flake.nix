cbee84c00e517f42af39f835a42f44de39d448b2
JohnathanBenge <johnathan@inkit.com>
2024-02-09 22:02:20 -0500

    update

--- flake.nix
+++ flake.nix
@@ -1,95 +1,44 @@
-
 {
-  description = "John's Config for NixOS and MacOS";
+  description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
-
-    home-manager.url = "github:nix-community/home-manager";
-    darwin = {
-      url = "github:LnL7/nix-darwin/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    }; 
-    disko = {
-      url = "github:nix-community/disko";
+    home-manager = {
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    kickstart-nix-nvim.url = "github:dp138504/kickstart-nix.nvim";
   };
-  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager, nixpkgs, disko, kickstart-nix-nvim } @inputs:
-    let
-      user = "johnathanbenge";
-      linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
-      darwinSystems = [ "aarch64-darwin" ];
-      forAllLinuxSystems = f: nixpkgs.lib.genAttrs linuxSystems (system: f system);
-      forAllDarwinSystems = f: nixpkgs.lib.genAttrs darwinSystems (system: f system);
-      forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) (system: f system);
-      devShell = system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = with pkgs; mkShell {
-          nativeBuildInputs = with pkgs; [ bashInteractive git age age-plugin-yubikey ];
-          shellHook = with pkgs; ''
-            export EDITOR=vim
-          '';
-        };
-      };
-    in
-    {
-      devShells = forAllSystems devShell;
-      darwinConfigurations = let user = "johnathanbenge"; in {
-        macos = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          specialArgs = inputs;
-          modules = [
-            nix-homebrew.darwinModules.nix-homebrew
-            home-manager.darwinModules.home-manager
-            {
-              nix-homebrew = {
-                enable = true;
-                user = "${user}";
-                taps = {
-                  "homebrew/homebrew-core" = homebrew-core;
-                  "homebrew/homebrew-cask" = homebrew-cask;
-                  "homebrew/homebrew-bundle" = homebrew-bundle; 
-                };
-                mutableTaps = false;
-                autoMigrate = true;
-              };
-            }
-            ./darwin
-          ];
-        };
-      };
-      nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system: nixpkgs.lib.nixosSystem {
-        system = system;
-        specialArgs = inputs;
-        modules = [
-          disko.nixosModules.disko
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${user} = import ./nixos/home-manager.nix;
-          }
-          ./nixos
+
+  outputs = { 
+    self, 
+    nix-darwin, 
+    nixpkgs, 
+    ... 
+  } @ inputs: let
+    inherit (self) outputs;
+  
+    # nix-darwin
+    darwinConfig = { pkgs, ... }: {
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+    };
+  in
+  {
+    darwinConfigurations = {
+      work-mbp = nix-darwin.lib.darwinSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [ 
+          darwinConfig
+          ./hosts/work/configuration.nix
         ];
-     });
+      };
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."simple".pkgs;
   };
 }
---
+++ hosts/work/home.nix
@@ -0,0 +1,97 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./neovim.nix
+    ./tmux.nix
+    ./zsh.nix
+  ];
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  home = {
+    username = "johnathanbenge";
+    homeDirectory = "/Users/johnathanbenge";
+    file.".zshrc".text = ''
+      autoload -U compinit && compinit
+    '';
+    stateVersion = "23.11"; 
+    packages = with pkgs; [
+
+      # General packages for development and system management
+      gh
+      git
+      go
+      istioctl
+      jq
+      killall
+      kubectl
+      kubernetes-helm
+      neofetch
+      nodePackages.pnpm
+      unixtools.watch
+      openssh
+      pandoc
+      pgformatter
+      postgresql_15
+      python310
+      qemu
+      ripgrep
+      terraform
+      trivy
+      vscode
+      wget
+      zip
+
+      # Encryption and security tools
+      awscli2
+      gnupg
+      libfido2
+      pinentry
+      yubikey-agent
+      yubikey-manager
+      yubikey-manager-qt
+      _1password-gui
+
+      # Media-related packages
+      flameshot
+      spotify
+      slack
+
+      # Text and terminal utilities
+      htop
+      hunspell
+      iftop
+      tmux
+      tree
+      unrar
+      unzip
+
+      # Container management and cloud tools
+      docker
+      docker-compose
+      ngrok
+      podman
+      tflint
+
+      # Miscellaneous development tools
+      actionlint
+      argocd
+      natscli
+    ];
+  };
+
+  programs.ssh = {
+    enable = true;
+    extraOptionOverrides = {
+      AddKeysToAgent = "yes";
+      IdentitiesOnly = "yes";
+      UseKeychain = "yes";
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
