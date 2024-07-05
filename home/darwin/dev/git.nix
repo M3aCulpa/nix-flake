59e6f4f0d3164c4771a5211916a2607d7a844b85
Brandon Epperson <brandon.epperson@protonmail.com>
2024-07-05 10:18:17 -0400

    remove: unused definitions

--- home/darwin/dev/git.nix
+++ home/darwin/dev/git.nix
@@ -1,15 +1,8 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   # `programs.git` will generate the config file: ~/.config/git/config
   # to make git use this config file, `~/.gitconfig` should not exist!
   #
   #    https://git-scm.com/docs/git-config#Documentation/git-config.txt---global
-  home.packages = with pkgs; [
-  ];
-
   programs.git = {
     enable = true;
     lfs.enable = true;
