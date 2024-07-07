0230d81d04c888eea289ddf662a777015c95b08f
Johnathan Benge <johnathanbenge@gmail.com>
2024-07-07 07:50:49 -0400

    fix: alacritty

--- home/darwin/alacritty/default.nix
+++ home/darwin/alacritty/default.nix
@@ -1,9 +1,10 @@
 {...}: {
   imports = [
-    ../../../modules/home/alacritty
+    ../../modules/home/alacritty
   ];
 
   programs.alacritty = {
     enable = true;
+    fontOffset = "{ x = 2, y = 2 }";
   };
 }
